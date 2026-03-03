34978d7adfe462b4fadec6f0ff9236ef8cab1c4f
njancodes <njansmailbox@gmail.com>
2026-03-03 11:44:09 +0530

    Completed the tests in step2

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ let tokens = [];
 let validJSON = false;
 //Lexer
 let braces_stk = [];
+let quotes_stk = [];
+let prop_part = "KEY";
 for(let character of contents){
 	if(character === '{'){
 		tokens.push("OPEN_BRACE");
@@ -17,20 +19,41 @@ for(let character of contents){
 	}else if(character === '}'){
 		tokens.push("CLOSED_BRACE");
 		braces_stk.pop()	
-	}	
+	}else if(character === '"'){
+		if(quotes_stk.length > 0){ 
+			quotes_stk.pop();	
+			tokens.push(prop_part);
+		}else{
+			quotes_stk.push('"');
+		}	
+	}else if(character == ':'){
+		prop_part = "VALUE";	
+	}else if(character == ','){
+		prop_part = "KEY"	
+		tokens.push("COLON");	
+	}
 }
-if(braces_stk.length === 0){
-	validJSON = true;
-}else{
+if(braces_stk.length !== 0 && quotes_stk.length !== 0){
 	console.log('Invalid JSON');
+	exit(1);
 }
 
-validJSON = false;
+validJSON = true;
 //Parser
+console.log(tokens);
 for(let i = 0; i < tokens.length; i++){
-	if(tokens[0] === "OPEN_BRACE"){
-		validJSON = true;	
+	if(tokens[0] !== "OPEN_BRACE"){
+		validJSON = false;
+		break;
 	}	
+	if(tokens[i] === "KEY" && tokens[i+1] !== "VALUE" || tokens[i] === "VALUE" && (tokens[i+1] !== "KEY" && tokens[i+1] !== "COLON" && tokens[i + 1] !== "CLOSED_BRACE")){
+		validJSON = false;
+		break;
+	}
+	if(tokens[i] === "COLON" && tokens[i+1] !== "KEY"){
+		validJSON = false;
+		break;
+	}
 }
 
 if(validJSON){
